c9c81bcb12b740549aa73b4c3031da7dc1535321
jolenezheng <jolenezheng18@gmail.com>
2020-02-01 23:47:03 -0500

    styling :c

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,9 @@
-var file = 'receipt.png'
-module.exports.file = file;
+const itemNode = $('#alternative-template');
+let alternatives = [1, 2, 3];
+let numAlt = alternatives.length;
+
+for (let i = 0; i < numAlt; ++i) {
+  console.log('w')
+  $("#options-container").append(itemNode);
+  $("#options-container")[i].removeClass('p-none');
+}

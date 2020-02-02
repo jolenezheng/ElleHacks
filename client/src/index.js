e6ba01ca562ef44d32107fe779f6117f50fb8e33
jolenezheng <jolenezheng18@gmail.com>
2020-02-02 01:42:12 -0500

    ewfjkdgf

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 const itemNode = $('#alternative-template');
+var myFootprint = 0;
 var alternatives = [1, 2, 3];
 var numAlt = alternatives.length;
 var mylist = $("#options-container");
 
 console.log(mylist)
-for ( i = 0; i < numAlt; i++){
+for (i = 0; i < numAlt; i++) {
   let li = mylist.find('li')[i];
   console.log(li);
   mylist.find(li).clone().appendTo(mylist);
@@ -13,7 +14,14 @@ for ( i = 0; i < numAlt; i++){
 }
 
 // Populate file upload text
-$(".custom-file-input").on("change", function() {
+$(".custom-file-input").on("change", function () {
   var fileName = $(this).val().split("\\").pop();
   $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
+
+$('#test-btn').click(function () {
+  $.get('http://localhost:8080/receipt', function (data) {})
+    .done(function (response) {
+      console.log(response);
+    })
 });

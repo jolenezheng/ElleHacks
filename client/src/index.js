5ef167053ca47ed80c2715aa48c30d1eb649db18
jolenezheng <jolenezheng18@gmail.com>
2020-02-02 04:09:47 -0500

    finished frontend??

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,12 @@
 const itemNode = $('#alternative-template');
-var myFootprint = 0;
-var alternatives = [1, 2, 3];
+var alternatives = [1, 2, 3, 4, 5, 6];
 var numAlt = alternatives.length;
 var mylist = $("#options-container");
+var myFootprint = 0;
+var c02 = 0;
+var pkg = 0;
+var waste = 0;
 
-console.log(mylist)
-for (i = 0; i < numAlt; i++) {
-  let li = mylist.find('li')[i];
-  console.log(li);
-  mylist.find(li).clone().appendTo(mylist);
-
-  // $('#options-container > li').addClass('d-none');
-}
-
-// Populate file upload text
 $(".custom-file-input").on("change", function () {
   var fileName = $(this).val().split("\\").pop();
   $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
@@ -24,4 +17,23 @@ $('#test-btn').click(function () {
     .done(function (response) {
       console.log(response);
     })
+  mylist.removeClass('d-none');
 });
+
+$('.btn-outline-dark').click(function() {
+  let avg = parseInt($(this).parent().siblings('i').find('span').html().slice(0, -1));
+  co2 = avg + 18;
+  pkg = avg - 7;
+  waste = avg - 11;
+  myFootprint = ((co2 + pkg + waste) / 3).toFixed(0);
+  console.log(avg);
+  $('#co2-val-after').html(co2.toFixed(0) + "%");
+  $('#co2-val-after').removeClass('d-none');
+  $('#co2-val-after').siblings('i').removeClass('d-none');
+  $('#pkg-val-after').html(pkg.toFixed(0) + "%");
+  $('#pkg-val-after').removeClass('d-none');
+  $('#pkg-val-after').siblings('i').removeClass('d-none');
+  $('#waste-val-after').html(waste.toFixed(0) + "%");
+  $('#waste-val-after').removeClass('d-none');
+  $('#waste-val-after').siblings('i').removeClass('d-none');
+})
